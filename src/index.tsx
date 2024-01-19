4e9146fc77fe96f9ccd6709ccc991a6b7b32eca3
Anthony Shuker <anthony@emobe.uk>
2024-01-19 20:21:04 +0100

    Add MUI

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,18 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import NotFound from './pages/NotFound';
+import Button from '@mui/material/Button';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hi</div>,
+    element: <div>Hi<Button>Hello</Button></div>,
     errorElement: <NotFound />
   }
 ]);
